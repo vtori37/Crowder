7e5a5041aa7294d3a88d2758553e166d0cc4b09c
luis <bluerizabiz@gmail.com>
2022-05-17 19:52:09 -0500

    routes for comment, post and users complete

--- controllers/api/Reaction_routes.js
+++ controllers/api/Reaction_routes.js
@@ -1,5 +1,5 @@
-const router = require ('express').Router();
-const { Reaction } = require('../../models');
+// const router = require ('express').Router();
+// const { Reaction } = require('../../models');
 
 
 
