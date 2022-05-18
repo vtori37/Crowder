ce815c5b447dce1029dd11380947fecec79b161b
Tori Acer <vrice360@gmail.com>
2022-05-17 21:19:23 -0400

    handlebars edit

--- server.js
+++ server.js
@@ -14,8 +14,20 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// eventually connect routes to server
-// app.use(routes);
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars({
+  layoutsDir: `$(__dirname)/views/layouts`
+}));
+// app.engine('handlebars', hbs.engine);
+app.use(express.static(`public`));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// eventually connect routes to server and turns on routes
+app.use(routes);
 
 // sync sequelize models to the db, initiate server
 sequelize.sync({ force: false }).then(() => {
