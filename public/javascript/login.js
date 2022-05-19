5ef0562ab8e8fe26d5be928ec97990554d268334
Tori Acer <vrice360@gmail.com>
2022-05-19 02:09:50 -0400

    updates to javacript

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -7,7 +7,16 @@ function Menu(e) {
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
+  const username = document.querySelector('#username-login').value.trim();
   const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login')
+  const password = document.querySelector('#password-login').value.trim();
 
+  if (username || email && password) {
+    const response = await fetch('api/users', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {'Content-Type': 'application/json'}
+    
+    })
+  }
 }
---
+++ public/javascript/user_signup.js
@@ -0,0 +1,13 @@
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('api/users', {
+      
+    })
+  }
+}
