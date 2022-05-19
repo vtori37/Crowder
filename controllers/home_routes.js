0eb71b723a20bd736634f43726d50025ced1f753
jrj-sys <jamesjamel99@gmail.com>
2022-05-19 02:22:45 -0500

    dashboard routes

--- controllers/home_routes.js
+++ controllers/home_routes.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { Event, Post, Comment } = require('../models');
 
-// Login
+// landing
+router.get('/', (req, res) => {
+  Event.findAll({
+    attributes: ['id', 'event_title', 'event_summary', 'event_url', 'event_start', 'event_end', 'event_image'],
+    include: [{
+      model: Post,
+      attributes: ['id', 'title', 'post_text', 'user_id'],
+      include: {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at']
+      }
+    }]
+  })
+  .then(dbEventData => {
+    const events  = dbEventData.map(event => event.get({ plain: true }));
+    res.render('landing_page', {
+      events
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
+});
+
+// login
+router.get('/login', (req, res) => {
+  res.render('login_signup');
+});
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-// const dashboardRoutes = require('./dashboard_routes')
-// const groupRoutes = require('./group_routes')
-// const homepageRoutes = require('./home_routes');
+const dashboardRoutes = require('./dashboard_routes')
+const groupRoutes = require('./group_routes')
+const homeRoutes = require('./home_routes');
 
-// router.use('/', homepageRoutes);
-// router.use('/dashboard', dashboardRoutes)
-// router.use('/group', groupRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes)
+router.use('/group', groupRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
