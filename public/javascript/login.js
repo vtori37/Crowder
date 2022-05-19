a6e5046dde3166d855fe47aa7b63e8879dc5e714
Tori Acer <vrice360@gmail.com>
2022-05-19 18:35:02 -0400

    change to login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -7,10 +7,10 @@ function Menu(e) {
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  const username = document.querySelector('#user-login').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (username && password) {
+  if (email && password) {
     const response = await fetch('api/users', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
