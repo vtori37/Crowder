666676049aced6855660ab375bca96e658012a30
Tori Acer <vrice360@gmail.com>
2022-05-19 00:45:17 -0400

    updates to js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -3,3 +3,7 @@ function Menu(e) {
 
   e.name === 'menu' ? (e.name = "close", list.classList.add('top-[80px]'), list.classList.add('opacity-100')) : (e.name = "menu", list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'))
 }
+
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+}
